fa78e2ed30cd4bb7609e58ffd42b6660a3b86b5c
Anwesha-Bhattacharjee <mohor122@gmail.com>
2025-01-11 11:51:48 +0530

    changes to run test on grid

---
+++ src/test/java/com/avi/SeleniumGrid1/SeleniumGridTest.java
@@ -0,0 +1,37 @@
+package com.avi.SeleniumGrid1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.io.ObjectInputFilter.Config;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+public class SeleniumGridTest {
+    public static void main(String[] args) throws InterruptedException, MalformedURLException {
+        // Define the URL of the Selenium Grid Hub
+    		URL hubUrl = new URL("http://192.168.0.102:4444/");
+
+            // Define desired capabilities for the browser
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setBrowserName("chrome");
+
+            // Initialize the RemoteWebDriver with the Hub URL and capabilities
+            WebDriver driver = new RemoteWebDriver(hubUrl, capabilities);
+            
+    	
+        
+
+        // Your test code here
+        driver.get("https://www.google.com");
+        System.out.println("Title: " + driver.getTitle());
+        
+        Thread.sleep(10000); // Buying enough time for me to check on which port it's running
+
+        // Close the browser
+        driver.quit();
+    }
+}
